ac7519f2429ded52252310867bd0daa2cd68daa3
maksymchak <maksym4ak@gmail.com>
2019-09-09 15:34:24 +0300

    Updates

---
+++ precache-manifest.c1c84ceae9454e90e5ef1beafa7b86fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3783775e44f12f61d6287760e66eb20a",
+    "url": "/movies-lib/index.html"
+  },
+  {
+    "revision": "9c7336f32708f71bc57e",
+    "url": "/movies-lib/static/css/main.c8290eab.chunk.css"
+  },
+  {
+    "revision": "5885b917469127cdb9fb",
+    "url": "/movies-lib/static/js/2.eb780407.chunk.js"
+  },
+  {
+    "revision": "9c7336f32708f71bc57e",
+    "url": "/movies-lib/static/js/main.abc3c68d.chunk.js"
+  },
+  {
+    "revision": "c8735d1028ef5b62fb91",
+    "url": "/movies-lib/static/js/runtime~main.a38139ad.js"
+  },
+  {
+    "revision": "b2849ae5466008ff37929c56b04dbb7d",
+    "url": "/movies-lib/static/media/new-logo.b2849ae5.png"
+  }
+]);
